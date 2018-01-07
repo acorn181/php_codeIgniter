3df87e3714ca8e2a981678e296f900706cd36ea1
acorn181 <masa.ichi.181@gmail.com>
2018-01-07 13:35:50 +0900

    ルーティングの修正

--- application/views/pages/login.php
+++
@@ -1,7 +0,0 @@
-<div id="login-panel">
-    <form action="users/session.php" method="POST">
-        ID : <input type="text" name="user_id" value=""><br>
-        パスワード : <input type="text" name="password" value=""><br>
-        <button type="submit">ログイン</button>
-    </form>
-</div>
