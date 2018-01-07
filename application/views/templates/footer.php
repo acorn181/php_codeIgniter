7c4c43bfccabb530cb165ae65283246920c47554
acorn181 <masa.ichi.181@gmail.com>
2018-01-07 17:20:17 +0900

    コピーライト修正

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
         <div class="copyright">
-            <em>&copy; 2017 masahich.com</em>
+            <em>&copy; 2017-<?php echo date('Y'); ?> masahich.com</em>
         </div>
     </body>
 </html>
